e85af8cd17e69f58fe663bd0ba20d4b5ab550679
DaBiggestPhrog <161621840+DaBiggestPhrog@users.noreply.github.com>
2024-04-17 15:03:20 +0000

    zad 3_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,27 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    // Wczytanie znaku
+    System.out.print("Podaj znak do narysowania obwodu kwadratu: ");
+    char znak = scanner.next().charAt(0);
+    // Wczytanie rozmiaru
+
+    System.out.print("Podaj rozmiar kwadratu: ");
+    int rozmiar = scanner.nextInt();
+
+    // Rysowanie kwadratu
+    for (int i = 1; i <= rozmiar; i++) {
+      for (int j = 1; j <= rozmiar; j++) {
+        if (i == 1 || i == rozmiar || j == 1 || j == rozmiar) {
+          System.out.print(znak + " ");
+        } else {
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
+    }
+  }
 }
